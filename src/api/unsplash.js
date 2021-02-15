bb77a785b399fb451d894acf337c31b2f6642e43
CaeRoberts <roberts.caeron@me.com>
2021-02-15 19:24:08 +0000

    V1
    V1 - shows initial search results using an unsplash API

---
+++ src/api/unsplash.js
@@ -0,0 +1,9 @@
+import react from "react";
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: "Client-ID buHfJPa8olyFRiE74SvsIspNIySpZT_FrR9u9tm6XJM",
+  },
+});
